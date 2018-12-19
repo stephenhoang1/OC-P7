535ed312521aa0fb1bc4d0210cd31f5f5afe1a75
stephenhoang1 <s.hoang17@gmail.com>
2018-12-18 17:11:57 -0800

    save before i try to refactor code to make it cleaner

--- script.js
+++ script.js
@@ -37,28 +37,211 @@ let markersArray = [];
           infoWindow.open(map);
           map.setCenter(pos);
 
+
+          // EXAMPLE START
+          var globalRestaurants;
+
+          // add review average to a copy of json data
+          function augmentRestaurantsData(restaurants) {
+            // for each restaurant add new calculated info
+            return restaurants.map((place) => ({
+              ...place,
+              // avarage rating is sum of all ratings / number of ratings
+              averageRating: place.ratings.reduce(
+                (total, curr) => curr.stars + total, 0) / place.ratings.length
+            }))
+          }
+
+          // fake render
+          function renderReviewList(restaurants) {
+            // ...create listings here
+          }
+
+          // your star filter
+          function filterByAvgStars(restaurants, min, max) {
+            return restaurants.filter(place => {
+              place.averageRating >= min &&
+              place.averageRating <= max
+            })
+          }
+
+          // new cleaner fetch
+          function getRestaurants() {
+            return fetch('restaurants.json')
+              .then(response => response.json())
+          }
+
+          function onInit() {
+            getRestaurants()  // fetch data
+              .then(augmentRestaurantsData) // add averages to json data.
+              .then(data => globalRestaurants = data) // add this data to a new variable.
+              .then(renderReviewList); // create restaurant listings from this data.
+          }
+
+          function onAvgStarsChange(min, max) {
+              if(!globalRestaurants) { //if globalRestaurants is empty
+                getRestaurants() // fetch data
+                  .then(augmentRestaurantsData) // add averages to json data.
+                  .then(data => { // add this data to a new variable.
+                    globalRestaurants = data;
+                    return data;
+                  })
+                  .then(data => filterByAvgStars(data, min, max)) // filter the data
+                  .then(renderReviewList); //  // create restaurant listings from this data.
+              }
+              var filtered = filterByAvgStars(globalRestaurants, min, max) //create filtered data
+              renderReviewList(filtered); // render new list using this data
+          }
+
+          // EXAMPLE END
+
+
+
           // *** FETCH START ***
           fetch('restaurants.json')
             .then(response => response.json())
             .then(data => {
 
+              /**
+               * add meta data for each restaurant
+               * PS not sure this codeworks i winged it :)
+               */
+              function augmentRestaurantsData(restaurants) {
+                // for each restaurant add new calculated info
+                return restaurants.map((place) => ({
+                  // make a new array of all the restaurants
+                  ...place,
+
+                  // avarage rating is sum of all ratings / number of ratings
+                  averageRating: place.ratings.reduce(
+                    (total, curr) => curr.stars + total, 0) / place.ratings.length
+                }))
+              }
+              // use this to render, maybe save it so you can use it later
+              // without recalling the fetch?
+              var dataWithAvgRatings = augmentRestaurantsData(data);
+
+
+
+
+              // *** INSERT MODAL DATA ***
+
+              function addModalData(restaurant) {
+                // image
+                    var modalImage = document.getElementById('m-img')
+                    var steetViewImg = `https://maps.googleapis.com/maps/api/streetview?size=254x254&location=${restaurant.lat},${restaurant.long}&fov=90&heading=235&pitch=10&key=AIzaSyAFAWdSJ9-ChPm_9XKsXG2aLGPDKc3aVfc`
+                    modalImage.src = steetViewImg
+
+                // name
+                    var modalRestName = document.getElementById('m-rest-name')
+                    modalRestName.innerHTML = restaurant.restaurantName
+
+                // number of reviews
+                    var modalNumberOfReviews = document.getElementById("m-number-of-reviews")
+                    modalNumberOfReviews.innerHTML = restaurant.ratings.length + " reviews"
+
+                // priceRange
+                    var modalPriceRange = document.getElementById('m-price')
+                    modalPriceRange.innerHTML = restaurant.priceRange
+
+                // cusine
+                    var modalCusine = document.getElementById('m-cusine')
+                    modalCusine.innerHTML = restaurant.cusine
+
+                // address
+                    var modalAddress = document.getElementById('m-address')
+                    modalAddress.innerHTML = restaurant.address
+
+
+                // review stars
+
+                    // make the average
+                    var count = 0;
+                    for(var y = 0; y < restaurant.ratings.length; y++) {
+                    count += restaurant.ratings[y].stars
+                    }
+                    starAverage = count / restaurant.ratings.length
+
+
+                    // clear existing stars
+                    document.getElementById("m-stars").innerHTML = ""
+
+                    // add the stars to the modal
+                    for(var x = 0; x < Math.round(starAverage); x++) {
+                      var modalStarsWrapper = document.getElementById("m-stars")
+                      var actualStar = document.createElement("i")
+                      actualStar.setAttribute('class', 'fas fa-star')
+                      modalStarsWrapper.prepend(actualStar)
+                    }
+
+                // opening times
+                  // *** ADD THIS FUNCTION WHEN YOU USE GOOGLE PLACES api ***
+
+                function createReview(n) {
+
+                  $('#m-reviews-container').empty();
+
+                  for (var x = 0; x < n; x++) {
+                    // 1. CREATE
+                    var userReview = document.createElement("div")
+                    userReview.setAttribute('class', 'row user-review')
+                    var user = document.createElement("div")
+                    user.setAttribute('class', 'user col-3')
+                    var review = document.createElement("div")
+                    review.setAttribute('class', 'review col-9')
+                    var name = document.createElement("div")
+                    name.setAttribute('class', 'name')
+                    var starsWrapper = document.createElement("div")
+                    starsWrapper.setAttribute('class', 'm-stars-wrapper stars')
+                    var reviewComment = document.createElement("p")
+                    reviewComment.setAttribute('class', 'review-comment')
+
+                    // 2. ARRANGE
+                    user.append(name)
+                    userReview.append(user)
+                    review.append(starsWrapper, reviewComment)
+                    userReview.append(review)
+
+                    var reviewsContainer = document.getElementById('m-reviews-container')
+                    reviewsContainer.append(userReview)
+
+                    // 3. INSERT
+
+                    reviewComment.innerHTML = restaurant.ratings[x].comment
+
+                    // stars
+                    for (var y = 0; y < restaurant.ratings[x].stars; y++) {
+                      var actualStars = document.createElement("i")
+                      actualStars.setAttribute('class', 'fas fa-star')
+                      starsWrapper.prepend(actualStars)
+                    }
+                  }
+                }
+
+                createReview(restaurant.ratings.length)
+
+              }
+
+              // CREATE LISTING
+
+              // function createListing()
+
               data.forEach((x, i) => {
-
-              // check x for any undefined, then filter them out
-              // use filter functions
-
-              // x.filter(y => undefined)
 
               var index = i + 1
 
               addMarker({lat: x.lat, lng: x.long}, "red", x, index)
-
 
                 // 1. CREATE
 
                 // make div
                 var listing = document.createElement("div")
                 listing.setAttribute('class', 'listing')
+
+                listing.addEventListener('click', function() {
+                  $("#myModal").modal()
+                  addModalData(x)
+                })
 
                 // make listing-avatar div
                 var listingAvatar = document.createElement("div")
@@ -74,8 +257,8 @@ let markersArray = [];
                   listingTitle.setAttribute('class', 'listing-title')
 
                   // reviews
-                  var reviews = document.createElement("div")
-                  reviews.setAttribute('class', 'reviews')
+                  var reviewAverage = document.createElement("div")
+                  reviewAverage.setAttribute('class', 'review-avg')
 
                     // stars
                     var stars = document.createElement("div")
@@ -114,10 +297,10 @@ let markersArray = [];
                     listingAvatar.append(picture)
 
                     // put divs into reviews div
-                    reviews.append(stars, bulletReviews, numberOfReviews)
+                    reviewAverage.append(stars, bulletReviews, numberOfReviews)
 
                     // put divs into the listing-info
-                    listingInfo.append(listingTitle, reviews, listingPrice, bulletPriceCusine, listingCusine)
+                    listingInfo.append(listingTitle, reviewAverage, listingPrice, bulletPriceCusine, listingCusine)
 
                     // put everything inside the listing div
                     listing.append(listingAvatar, listingInfo)
@@ -147,10 +330,9 @@ let markersArray = [];
                 listingTitle.innerHTML = x.restaurantName
                 listingTitle.prepend(`${index}. `)
 
-
             // review score
                 // makeAverage()
-                createStars(x.average)
+                createStars(x.average, stars)
 
             // add number of reviews function Here
                 numberOfReviews.innerHTML = x.ratings.length + " reviews"
@@ -161,18 +343,22 @@ let markersArray = [];
             // add cusine here
                 listingCusine.innerHTML = x.cusine
 
+
             // helper functions:
-                function createStars(n) {
+                function createStars(n, id) {
                   for(var x = 0; x < Math.round(n); x++) {
                     var actualStars = document.createElement("i")
                     actualStars.setAttribute('class', 'fas fa-star')
-                    stars.append(actualStars)
+                    id.prepend(actualStars)
                   }
                 }
 
-              })
+              }) // END OF LOOP
 
             })  //END OF JSON FETCH
+
+
+
 
 
         }, function() {
@@ -209,6 +395,7 @@ let markersArray = [];
             });
 
             listNumber = listNumber ? `${listNumber}. ` : ""
+            // console.log(listNumber)
 
             var restInfo = `
             <div class="listing">
